661c18b870c7cc6591316ce0d4cc8bf919bf68fb
YOGEESH K P <148324344+yogee555@users.noreply.github.com>
2024-06-18 11:50:09 +0530

    Add files via upload

---
+++ Fizz Buzz.py
@@ -0,0 +1,10 @@
+# Fizz Buzz
+for number in range(50):
+    if number %3==0 and number %5==0:
+        print("Fizz Buzz")
+    elif number %3==0:
+        print("Fizz")
+    elif number %5==0:
+        print("Buzz")
+    else:
+        print(number)
